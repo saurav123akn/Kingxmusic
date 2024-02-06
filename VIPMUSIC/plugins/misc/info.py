863165b53da75525bcfde528297a76a9a95df179
ㅤㅤㅤㅤ <133571161+CoderPrit@users.noreply.github.com>
2024-02-07 05:28:49 +0530

    Update info.py

--- VIPMUSIC/plugins/misc/info.py
+++ VIPMUSIC/plugins/misc/info.py
@@ -10,11 +10,11 @@ from typing import Union, Optional
 import random
 
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://telegra.ph/file/19fa6b8b54ddb301839d3.jpg",
+    "https://telegra.ph/file/5ea3358c7090f968eb73f.jpg",
+    "https://telegra.ph/file/4fa8d5906b60041a27fe9.jpg",
+    "https://telegra.ph/file/b7337635e4c01554b4215.jpg",
+    "https://telegra.ph/file/2a20634d509d79d63ba88.jpg",
 ]
 
 # --------------------------------------------------------------------------------- #
